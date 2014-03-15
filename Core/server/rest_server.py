33aab244eea1a59feda5a35169586113b8f43e77
Alexander Dorodnykh <ruskagerot@gmail.com>
2014-03-15 11:07:11 +0400

    ready to host

--- Core/server/rest_server.py
+++ Core/server/rest_server.py
@@ -65,6 +65,4 @@ def uploaded_file(filename):
                                filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-"host='0.0.0.0', port=5001,"
+    app.run(host='0.0.0.0')

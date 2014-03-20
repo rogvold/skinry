2cebe28ed360b7bbfa379f93413a880b10adc16d
aartikov <akmal1994@yandex.ru>
2014-03-20 23:12:27 +0400

    Added deleting wrong keypoints

--- Core/server/skinscan.py
+++ Core/server/skinscan.py
@@ -1,6 +1,7 @@
 import sys
 import cv2
 import cv2.cv
+import cv
 import numpy as np
 import math
 from matplotlib import pyplot as plt
@@ -46,7 +47,7 @@ def crop_limbs(masked_image, eyes_coordinates, nose_coordinates, mouth_coordinat
     to_x = from_x + nose_coordinates[2]
     masked_image[from_y:to_y, from_x:to_x] = black
 
-    width = mouth_coordinates[3]
+    width = mouth_coordinates[2]
     alpha = 0.2
 
     from_y = mouth_coordinates[1]
@@ -55,8 +56,8 @@ def crop_limbs(masked_image, eyes_coordinates, nose_coordinates, mouth_coordinat
     to_x = from_x + mouth_coordinates[2] + 2 * alpha * width
     masked_image[from_y:to_y, from_x:to_x] = black
 
-    width = max(eyes_coordinates[0][3], eyes_coordinates[1][3])
-    height = max(eyes_coordinates[0][2], eyes_coordinates[1][2])
+    width = max(eyes_coordinates[0][2], eyes_coordinates[1][2])
+    height = max(eyes_coordinates[0][3], eyes_coordinates[1][3])
     alpha = 0.1
     beta = 0.3
 
@@ -80,26 +81,32 @@ def get_otsu(image, thresh_val, max_val):
     r, image = cv2.threshold(image, thresh_val, max_val, cv2.THRESH_TRUNC)
     return image
 
+def is_close(x, y, x1, y1, x2, y2, dist):
+    h = 0
+    sc = (x2 - x1) * (x - x1) + (y - y1) * (y2 - y1)
+    if sc <= 0:
+        h = math.sqrt((x - x1)**2 + (y - y1)**2)
+    else:
+        if (x2 - x1)**2 + (y2 - y1)**2 <= sc:
+            h = math.sqrt((x - x2)**2 + (y - y2)**2)
+        else:
+            h = abs(((x1 - x2) * (y - y1) + (y2 - y1) * (x - x1)) / (math.sqrt((x2 - x1)**2 + (y2 - y1)**2)))
+    return h < dist
+
 def check_border(x, y, result_points):
     for i in range(0, len(result_points) - 1):
         x1 = result_points[i][0]
         x2 = result_points[i + 1][0]
         y1 = result_points[i][1]
         y2 = result_points[i + 1][1]
-        h = abs(((x1 - x2) * (y - y1) + (y2 - y1) * (x - x1)) / (math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))))
-        if h < 15.0:
-            #print h
-            #print str(x) + ' ' + str(y)
+        if is_close(x, y, x1, y1, x2, y2, dist=25.0):
             return True
 
     x1 = result_points[0][0]
     x2 = result_points[len(result_points) - 1][0]
     y1 = result_points[0][1]
     y2 = result_points[len(result_points) - 1][1]
-    h = abs(((x1 - x2) * (y - y1) + (y2 - y1) * (x - x1)) / (math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))))
-    if h < 15.0:
-        #print h
-        #print str(x) + ' ' + str(y)
+    if is_close(x, y, x1, y1, x2, y2, dist=25.0):
         return True
 
     return False
@@ -124,35 +131,41 @@ def delete_unused_keypoints(key_points, result_points, eyes_coordinates, nose_co
            new_kp.append(kp)
            continue
 
-        from_y = nose_coordinates[1]
-        to_y = from_y + nose_coordinates[3]
-        from_x = nose_coordinates[0]
-        to_x = from_x + nose_coordinates[2]
-        if x > from_x and x < to_x and y > from_y and y < to_y:
-            new_kp.append(kp)
-            continue
-
-        width = mouth_coordinates[3]
+        width = nose_coordinates[2]
+        height = nose_coordinates[3]
         alpha = 0.2
 
-        from_y = mouth_coordinates[1]
-        to_y = from_y + mouth_coordinates[3]
-        from_x = mouth_coordinates[0] - alpha * width
-        to_x = from_x + mouth_coordinates[2] + 2 * alpha * width
-        if x > from_x and x < to_x and y > from_y and y < to_y:
+        from_y = nose_coordinates[1] - alpha * height
+        to_y = from_y + nose_coordinates[3] + 2 * alpha * height
+        from_x = nose_coordinates[0] - alpha * width
+        to_x = from_x + nose_coordinates[2] + 2 * alpha * width
+        if to_x > x > from_x and to_y > y > from_y:
             new_kp.append(kp)
             continue
 
-        width = max(eyes_coordinates[0][3], eyes_coordinates[1][3])
-        height = max(eyes_coordinates[0][2], eyes_coordinates[1][2])
-        alpha = 0.1
-        beta = 0.3
+        height = mouth_coordinates[3]
+        width = mouth_coordinates[2]
+        beta = 0.2
+        alpha = 0.25
+
+        from_y = mouth_coordinates[1] - beta * height
+        to_y = from_y + mouth_coordinates[3] + 2 * beta * height
+        from_x = mouth_coordinates[0] - alpha * width
+        to_x = from_x + mouth_coordinates[2] + 2 * alpha * width
+        if to_x > x > from_x and to_y > y > from_y:
+            new_kp.append(kp)
+            continue
+
+        width = max(eyes_coordinates[0][2], eyes_coordinates[1][2])
+        height = max(eyes_coordinates[0][3], eyes_coordinates[1][3])
+        alpha = 0.15
+        beta = 0.35
 
         from_y = eyes_coordinates[0][1] - beta * height
         to_y = from_y + eyes_coordinates[0][3] + 2 * beta * height
         from_x = eyes_coordinates[0][0] - alpha * width
         to_x = from_x + eyes_coordinates[0][2] + 2 * alpha * width
-        if x > from_x and x < to_x and y > from_y and y < to_y:
+        if to_x > x > from_x and to_y > y > from_y:
             new_kp.append(kp)
             continue
 
@@ -160,7 +173,7 @@ def delete_unused_keypoints(key_points, result_points, eyes_coordinates, nose_co
         to_y = from_y + eyes_coordinates[1][3] + 2 * beta * height
         from_x = eyes_coordinates[1][0] - alpha * width
         to_x = from_x + eyes_coordinates[1][2] + 2 * alpha * width
-        if x > from_x and x < to_x and y > from_y and y < to_y:
+        if to_x > x > from_x and to_y > y > from_y:
             new_kp.append(kp)
             continue
 
@@ -189,11 +202,10 @@ def delete_unused_keypoints(key_points, result_points, eyes_coordinates, nose_co
         color = original_image[y][x][2] - min_red
         size = kp.size - min_size
         point = 100 + 75 * (float(color) / length_red) + 50 * (float(size) / length_size)
-        #print point
         score += point
 
     score = int(score)
-    return key_points, score
+    return key_points, score, original_image
 
 def detect_deffects(file_name):
     new_file_name = 'uploads/' + file_name
@@ -205,7 +217,7 @@ def detect_deffects(file_name):
     roi = get_otsu(roi, 200, 255)
 
     key_points = sift(roi, contrast_threshold=0.02, edge_threshold=15, sigma=2)
-    key_points, score = delete_unused_keypoints(key_points, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates, roi, image)
+    key_points, score, image = delete_unused_keypoints(key_points, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates, roi, image)
     result_image = cv2.drawKeypoints(image, key_points, flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     print 'total score - ' + str(score)

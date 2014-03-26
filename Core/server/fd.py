5e745fd82176d2dcb71ffb7410c5f788694484f3
AlmazNasibullin <nasibyllin.almaz@phystech.edu>
2014-03-26 18:13:59 +0400

    update detect_face_and_organs

--- Core/server/fd.py
+++ Core/server/fd.py
@@ -20,7 +20,7 @@ def grahamscan(A):
 
     for i in range(2,n):
         j = i
-        while j > 1 and (rotate(A[P[0]], A[P[j - 1]], A[P[j]]) < 0): 
+        while j > 1 and (rotate(A[P[0]], A[P[j - 1]], A[P[j]]) < 0):
             P[j], P[j-1] = P[j-1], P[j]
             j -= 1
       
@@ -38,6 +38,7 @@ def grahamscan(A):
 
 def detect_face_and_organs(img):
     scale_factor = 1.1
+    H, W = img.shape[:2]
 
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
     eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
@@ -47,7 +48,15 @@ def detect_face_and_organs(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.equalizeHist(gray)
     
-    face = face_cascade.detectMultiScale(gray, scale_factor, 3, cv2.cv.CV_HAAR_SCALE_IMAGE, (30, 30))
+    to_do = True
+    for sf in [1.1, 1.05]:
+        if to_do == False:
+            break
+        for mn in [3, 5, 7]:
+            face = face_cascade.detectMultiScale(gray, sf, mn, cv2.cv.CV_HAAR_SCALE_IMAGE, (H / 8, W / 8))
+            if len(face) > 0:
+                to_do = False
+                break
 
     if len(face) == 0:
         raise ValueError("No face")
@@ -56,6 +65,12 @@ def detect_face_and_organs(img):
     for (x,y,w,h) in face:
         y -= 0.1 * (y + h / 2)
         h += 0.2 * (y + h / 2)
+        y = max(int(y), 0)
+        h = min(int(h), H - 1 - y)
+
+        if H * W > 10 * h * w:
+            break
+
         roi = img[y:y + h, x:x + w]
         roi_gray = gray[y:y + h, x:x + w]
 
@@ -85,22 +100,23 @@ def detect_face_and_organs(img):
 
         #define nose
         nose = nose_cascade.detectMultiScale(roi_gray, scale_factor, 5)
-        if len(nose) == 0:
-            raise ValueError("No nose")
+        #if len(nose) == 0:
+        #    raise ValueError("No nose")
         for (nx, ny, nw, nh) in nose:
             if (nx > eyes_coordinates[0][0] + int(eyes_coordinates[0][2] * 0.3) and
-                nx < eyes_coordinates[1][0] and ny > eyes_coordinates[0][1] and 
+                nx < eyes_coordinates[1][0] and ny > (eyes_coordinates[0][1] + 
+                int(0.6 * eyes_coordinates[0][3])) and
                 abs(ny - eyes_coordinates[0][1] - eyes_coordinates[0][3]) < h / 7):
                 nose_coordinates = (nx, ny, nw, nh)
                 break
         
         #define mouth
         mouth = mouth_cascade.detectMultiScale(roi_gray, scale_factor, 5)
-        if len(mouth) == 0:
-            raise ValueError("No mouth")
+        #if len(mouth) == 0:
+        #    raise ValueError("No mouth")
         for (mx, my, mw, mh) in mouth:
             if (my > nose_coordinates[1] + int(nose_coordinates[3] * 0.7) and mx > eyes_coordinates[0][0]
-                and mx < eyes_coordinates[1][0] and mx + mw > eyes_coordinates[1][0]):
+                and mx < eyes_coordinates[1][0]  and mx + mw > eyes_coordinates[1][0]):
                 mouth_coordinates = (mx, my, mw, mh)
                 break
 
@@ -109,7 +125,7 @@ def detect_face_and_organs(img):
 
 def define_contours(img):
     train = pd.read_csv('train.csv')[:3000]
-    X_train =  np.asarray(train[range(0, 3)])
+    X_train = np.asarray(train[range(0, 3)])
     Y_train = np.asarray(train[[3]]).ravel()
 
     estimator = KNeighborsClassifier(n_neighbors = 3, weights = 'distance')
@@ -131,7 +147,7 @@ def define_contours(img):
         for k in range(int(0.5 * w), w):
             if res[k] == 1:
                 count += 1
-                if count > 40:
+                if count > 20:
                     x2 = k
             else:
                 count = 0
@@ -140,7 +156,7 @@ def define_contours(img):
         for k in range(int(w / 2), -1, -1):
             if res[k] == 1:
                 count += 1
-                if count > 40:
+                if count > 20:
                     x1 = k
             else:
                 count = 0
@@ -160,9 +176,6 @@ def define_contours(img):
     points.append((int(0.6 * w), h - 1))
 
     points = grahamscan(points)
-
-    for i in range(0, len(points)):
-        cv2.line(img, points[i - 1], points[i],  (0, 0, 255), 2)
     
     return points
 
@@ -174,8 +187,5 @@ def get_param(file_name):
     face_image, eyes_coordinates, nose_coordinates, mouth_coordinates = detect_face_and_organs(img)
     # 'result_points' - array of contour's points
     result_points = define_contours(face_image)
-    
-    #result_file_name = "result.jpg"
-    #cv2.imwrite(result_file_name, face_image)
     
     return result_points, eyes_coordinates, nose_coordinates, mouth_coordinates, face_image

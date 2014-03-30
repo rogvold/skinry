2da4c17c335050efe4baf338675f393e907f7ed8
aartikov <akmal1994@yandex.ru>
2014-03-30 16:35:43 +0400

    [Core] sift grid search, small bugs fix

--- Core/server/skinscan.py
+++ Core/server/skinscan.py
@@ -20,16 +20,6 @@ def draw_face(image, result_points):
         cv2.line(image, result_points[i - 1], result_points[i],  (0, 0, 255), 2)
     return image
 
-def sift(image, contrast_threshold, edge_threshold, sigma):
-    sift = cv2.SIFT(0, 5, contrast_threshold, edge_threshold, sigma)
-    key_points = sift.detect(image, None)
-    return key_points
-
-def get_otsu(image, thresh_val, type):
-    #image = cv2.medianBlur(image,5)
-    r, image = cv2.threshold(image, thresh_val, 255, type)
-    return image
-
 def crop_face(image, result_points):
     mask = np.zeros(image.shape, dtype=np.uint8)
     white = (255, 255, 255)
@@ -66,7 +56,6 @@ def crop_limbs(masked_image, eyes_coordinates, nose_coordinates, mouth_coordinat
     masked_image[from_y:to_y, from_x:to_x] = black
 
     return masked_image
-
 
 def get_coords(array, alpha, beta, width, height):
     from_y = array[1] - beta * height
@@ -108,9 +97,11 @@ def check_border(x, y, result_points, dist):
 def delete_unused_keypoints(image, key_points, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates):
     new_kp = []
     dist = 0.03 * max(image.shape[0], image.shape[1])
+    min_size = 0.007 * max(image.shape[0], image.shape[1])
+    max_size = 0.055 * max(image.shape[0], image.shape[1])
 
     for kp in key_points:
-        if kp.size < 5 or kp.size > 40:
+        if kp.size < min_size or kp.size > max_size:
             new_kp.append(kp)
             continue
 
@@ -161,6 +152,24 @@ def delete_unused_keypoints(image, key_points, result_points, eyes_coordinates, 
     key_points = list(set(key_points) - set(new_kp))
     return key_points
 
+def delete_repeating_points(good_points):
+    repeating_points = []
+
+    for kp1 in good_points:
+        for kp2 in good_points:
+            if kp1 != kp2:
+                x1 = int(kp1.pt[0])
+                y1 = int(kp1.pt[1])
+                x2 = int(kp2.pt[0])
+                y2 = int(kp2.pt[1])
+                dist = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+                if dist < kp1.size and kp1.size > kp2.size:
+                    repeating_points.append(kp1)
+
+    good_points = list(set(good_points) - set(repeating_points))
+
+    return good_points
+
 def get_score(original_image, key_points):
     max_red = 0
     min_red = 255
@@ -202,20 +211,52 @@ def get_score(original_image, key_points):
     score = int(score)
     return score
 
-def grid_otsu(roi, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates,
-              min_thresh_val, max_thresh_val, step, delta):
+def sift(image, contrast_threshold, edge_threshold, sigma):
+    sift = cv2.SIFT(0, 5, contrast_threshold, edge_threshold, sigma)
+    key_points = sift.detect(image, None)
+    return key_points
 
-    roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    thresh_val = min_thresh_val
+def get_otsu(image, thresh_val, type):
+    #image = cv2.medianBlur(image,5)
+    r, image = cv2.threshold(image, thresh_val, 255, type)
+    return image
+
+def grid_sift(image, octaves, contrast_thresholds, edge_thresholds, sigmas, delta):
     points = {}
     good_points = []
 
-    while thresh_val <= max_thresh_val:
+    for octave in octaves:
+        for contrast_threshold in contrast_thresholds:
+            for edge_threshold in edge_thresholds:
+                for sigma in sigmas:
+                    sift = cv2.SIFT(0, octave, contrast_threshold, edge_threshold, sigma)
+                    key_points = sift.detect(image, None)
+                    for kp in key_points:
+                        x = int(kp.pt[0])
+                        y = int(kp.pt[1])
+                        coords = (x,y)
+                        if not points.has_key(coords):
+                            points[coords] = [kp, 1]
+                        else:
+                            points[coords][1] += 1
+
+    for point in points.items():
+        if point[1][1] > delta:
+            good_points.append(point[1][0])
+
+    return good_points
+
+def grid_otsu(roi, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates,
+              threshs, delta):
+    points = {}
+    good_points = []
+
+    for thresh_val in threshs:
         new_roi = get_otsu(roi, thresh_val, type=cv2.THRESH_TRUNC)
 
         key_points = sift(new_roi, contrast_threshold=0.02, edge_threshold=10, sigma=1.6)
-        key_points = delete_unused_keypoints(new_roi, key_points, result_points, eyes_coordinates,
-                                             nose_coordinates, mouth_coordinates)
+        #key_points = delete_unused_keypoints(new_roi, key_points, result_points, eyes_coordinates,
+        #                                     nose_coordinates, mouth_coordinates)
         for kp in key_points:
             x = int(kp.pt[0])
             y = int(kp.pt[1])
@@ -225,28 +266,47 @@ def grid_otsu(roi, result_points, eyes_coordinates, nose_coordinates, mouth_coor
             else:
                 points[coords][1] += 1
 
-        thresh_val += step
-
     for point in points.items():
         if point[1][1] > delta:
             good_points.append(point[1][0])
 
-    repeating_points = []
-
-    for kp1 in good_points:
-        for kp2 in good_points:
-            if kp1 != kp2:
-                x1 = int(kp1.pt[0])
-                y1 = int(kp1.pt[1])
-                x2 = int(kp2.pt[0])
-                y2 = int(kp2.pt[1])
-                dist = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-                if dist < kp1.size and kp1.size > kp2.size:
-                    repeating_points.append(kp1)
-
-    good_points = list(set(good_points) - set(repeating_points))
-
     return good_points
+
+def sift_grid_search(roi, image, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates, **kwargs):
+    roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    roi = get_otsu(roi, **kwargs)
+
+    octaves = [5]
+    contrast_thresholds = [0.015, 0.02, 0.025]
+    edge_thresholds = [10]
+    sigmas = [1.2, 1.4, 1.6, 1.8]
+
+    key_points = grid_sift(roi, octaves, contrast_thresholds, edge_thresholds, sigmas, delta=4)
+
+    key_points = delete_unused_keypoints(roi, key_points, result_points, eyes_coordinates,
+                                         nose_coordinates, mouth_coordinates)
+
+    key_points = delete_repeating_points(key_points)
+
+    score = get_score(image, key_points)
+    result_image = cv2.drawKeypoints(image, key_points, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    return result_image, score
+
+def otsu_grid_search(roi, image, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates):
+    threshs = [160, 190, 200, 210]
+    roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+    key_points = grid_otsu(roi, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates, threshs=threshs,
+                           delta=1)
+    key_points = delete_unused_keypoints(roi, key_points, result_points, eyes_coordinates,
+                                         nose_coordinates, mouth_coordinates)
+    key_points = delete_repeating_points(key_points)
+
+    score = get_score(image, key_points)
+    result_image = cv2.drawKeypoints(image, key_points, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    return result_image, score
 
 def mono_search(roi, image, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates, **kwargs):
     roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
@@ -257,6 +317,7 @@ def mono_search(roi, image, result_points, eyes_coordinates, nose_coordinates, m
     key_points = sift(roi, contrast_threshold=0.02, edge_threshold=10, sigma=1.6)
     key_points = delete_unused_keypoints(roi, key_points, result_points, eyes_coordinates,
                                          nose_coordinates, mouth_coordinates)
+    key_points = delete_repeating_points(key_points)
 
     score = get_score(image, key_points)
     result_image = cv2.drawKeypoints(image, key_points, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
@@ -266,22 +327,17 @@ def mono_search(roi, image, result_points, eyes_coordinates, nose_coordinates, m
 
     return result_image, score
 
-def poly_search(roi, image, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates, **kwargs):
-
-    key_points = grid_otsu(roi, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates, **kwargs)
-    score = get_score(image, key_points)
-    result_image = cv2.drawKeypoints(image, key_points, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    return result_image, score
-
 def detect_deffects(file_name):
     new_file_name = 'uploads/' + file_name
     time = cv2.getTickCount()
     result_points, eyes_coordinates, nose_coordinates, mouth_coordinates, image = fd.get_param(new_file_name)
     roi = crop_face(image, result_points)
+    #roi = crop_limbs(roi, eyes_coordinates, nose_coordinates, mouth_coordinates)
 
-    result_image, score = poly_search(roi, image, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates,
-              min_thresh_val=150, max_thresh_val=220, step=10, delta=2)
+    result_image, score = otsu_grid_search(roi, image, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates)
+
+    #result_image, score = sift_grid_search(roi, image, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates,
+    #                                  thresh_val=200, type=cv2.THRESH_TRUNC)
 
     #result_image, score = mono_search(roi, image, result_points, eyes_coordinates, nose_coordinates, mouth_coordinates,
     #                                  thresh_val=200, type=cv2.THRESH_TRUNC)
@@ -297,13 +353,25 @@ def detect_deffects(file_name):
 
     return return_file_name, score
 
-#TODO sift(grid 2nd param 3-5, 3rd 0.02-0.05, 4th 8-15, 5th 1.2-2.0)
-#TODO nose increase width?
+#TODO refactor
+#TODO create libraries
+#TODO classes?
+#TODO sift grid decrease delta
+#TODO min and max size???
+#TODO combined grid
+#TODO limbs in one array
+#TODO time optimization
 #TODO binary thresholding?
 #TODO points alongside contour in the case of bad face recognition (clasterization?)
+#TODO play with contrast, brightness, blur
+#TODO no filter?
+#TODO FAST?
 #TODO real deffects near limbs? resize photo?
 #TODO cheecks, relief?
-#TODO play with contrast, brightness, blur
 #TODO genetic algorythm
+
+
+
+
 
 

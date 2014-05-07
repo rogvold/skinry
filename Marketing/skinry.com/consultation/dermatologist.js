e4636a78623a963f3e75a1ed9da5a1c0fa7e1708
illusionww <illusionww@gmail.com>
2014-05-07 12:26:09 +0400

    [site] pluso buttons, dermatologist php image upload

--- Marketing/skinry.com/consultation/dermatologist.js
+++ Marketing/skinry.com/consultation/dermatologist.js
@@ -4,6 +4,46 @@ $(function () {
     var applicationId = "BiixOUv8TBRRCc9PnScmyF2XMHRZhx2LfmvdqtvA";
     var javaScriptKey = "Sj5Vw02dRs3zI59caHeMQCEB9EXrNcsKPe0xkczc";
     Parse.initialize(applicationId, javaScriptKey);
+
+    /* variables */
+    var preview = $('#upl-img');
+    var status = $('#upl-status');
+    var percent = $('#upl-percent');
+    var bar = $('#upl-bar');
+
+    /* only for image preview */
+    $("#image").change(function(){
+        preview.fadeOut();
+        /* html FileRender Api */
+        var oFReader = new FileReader();
+        oFReader.readAsDataURL(document.getElementById("image").files[0]);
+        oFReader.onload = function (oFREvent) {
+            preview.attr('src', oFREvent.target.result).fadeIn();
+        };
+    });
+
+    /* submit form with ajax request */
+    $('#image-upload-form').ajaxForm({
+        /* set data type json */
+        dataType: 'json',
+        /* reset before submitting */
+        beforeSend: function() {
+            status.fadeOut();
+            bar.width('0%');
+            percent.html('0%');
+        },
+        /* progress bar call back*/
+        uploadProgress: function(event, position, total, percentComplete) {
+            var pVel = percentComplete + '%';
+            bar.width(pVel);
+            percent.html(pVel);
+        },
+        /* complete call back */
+        complete: function(data) {
+            preview.fadeOut(800);
+            status.html(data.responseJSON.status).fadeIn();
+        }
+    });
 });
 
 function getParameterByName(name) {
---
+++ Marketing/skinry.com/consultation/upload.php
@@ -0,0 +1,39 @@
+<?php
+header('Content-type: application/json');
+
+$valid_exts = array('jpeg', 'jpg', 'png', 'gif'); // valid extensions
+$max_size = 20 * 1024 * 1024; // max file size (20mb)
+$path = 'uploads/'; // upload directory
+
+$request_method = strtoupper(getenv('REQUEST_METHOD'));
+
+if ($request_method === 'POST') {
+    if (@is_uploaded_file($_FILES['image']['tmp_name'])) {
+        // get uploaded file extension
+        $ext = strtolower(pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION));
+        // looking for format and size validity
+        if (in_array($ext, $valid_exts)) {
+            if ($_FILES['image']['size'] < $max_size) {
+                // unique file path
+                $path = $path . uniqid() . '.' . $ext;
+                // move uploaded file from temp to uploads directory
+                if (move_uploaded_file($_FILES['image']['tmp_name'], $path)) {
+                    $status = 'Изображение загружено!';
+                } else {
+                    $status = 'Неизвестная ошибка!';
+                }
+            } else {
+                $status = 'Ошибка: размер файла больше 20Мб!';
+            }
+        } else {
+            $status = 'Ошибка: формат файлов должен быть: .jpeg, .jpg, .png или .gif!';
+        }
+    } else {
+        $status = 'Ошибка: файл не был выбран!';
+    }
+} else {
+    $status = 'Ошибка: Неправильный запрос!';
+}
+
+// echo out json encoded status
+echo json_encode(array('status' => $status));
